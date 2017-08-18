480ac322dfb076d92fd1dc5b2059b031040aa7ac
Kamil Naja <kamilnaja@gmail.com>
2017-08-19 01:11:33 +0200

    merge

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ angular.module('app', ['ngRoute']).controller('fvCtrl', function ($scope, $http)
   $scope.amount = 0;
   $scope.client = "";
   $scope.number = $scope.number++;
-
+  var fvs = [];
+  $scope.calculateBrutto = function () {
+    //zwróć brutto
+  }
   $scope.amountBrutto = $scope.amount + ($scope.amount * 23 / 100);
 
   $scope.saveDB = function () {
     //save amount, nr kolejny i client do db
     if ($scope.client && $scope.amount) {
       $http.post('/api/fvs', {
-        number: number++,
+        number: 10,
         client: $scope.client,
         amount: $scope.amount
       })
@@ -17,7 +20,7 @@ angular.module('app', ['ngRoute']).controller('fvCtrl', function ($scope, $http)
         //error
         $scope.fvs.unshift(fv);
       })
-    }
+  }
   };
   
 $http.get('http://localhost:3000/api/clients')
@@ -26,8 +29,9 @@ $http.get('http://localhost:3000/api/clients')
   })
  
   $http.get('http://localhost:3000/api/fvs')
-  .then(function (fvs) {
+  .then(function(fvs){
     $scope.fvs = fvs
+    console.log(fvs);
   })
 })
 //po kliknięciu zapisz - zapisz fv w db
--- models/client.js
+++
@@ -1,6 +0,0 @@
-var db = require('../db');
-var Client = db.model('Client', {
-  Name: {type: String, required: false },
-  Nip: {type: Number, required: false }
-});
-module.exports = Client;
